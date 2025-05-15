a531e6e7560b9ac90c00aca4b646ce5061046e52
Zubin Henner <9605+zubin@users.noreply.github.com>
2025-05-16 06:38:03 +1000

    fix: delivery attempt paths

--- lib/convoy/resources/delivery_attempt.rb
+++ lib/convoy/resources/delivery_attempt.rb
@@ -3,20 +3,20 @@ module Convoy
     include ApiOperations::Get
     include ApiOperations::List
 
-    def initialize(eventId = nil, id = nil, config = Convoy.config, **kwargs)
+    def initialize(event_delivery_id = nil, id = nil, config = Convoy.config, **kwargs)
+      @event_delivery_id = event_delivery_id
       @id = id
-      @eventId = eventId
       @config = config
-      
+
       super(**kwargs)
     end
 
     def resource_uri
       if @id.nil?
-        return "#{project_base_uri}/events/#{@eventId}/deliveryattempts"
+        return "#{project_base_uri}/eventdeliveries/#{@event_delivery_id}/deliveryattempts"
       end
 
-      "#{project_base_uri}/events/#{@eventId}/deliveryattempts/#{@id}"
+      "#{project_base_uri}/eventdeliveries/#{@event_delivery_id}/deliveryattempts/#{@id}"
     end
   end
 end
